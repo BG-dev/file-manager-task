441417f4717a1405f053c49839883d8f19affc77
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-14 02:00:43 +0300

    init: start file-manager task

---
+++ index.js
@@ -0,0 +1,10 @@
+import * as readline from "node:readline";
+
+function main(){
+    const readline = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+}
+
+main();
